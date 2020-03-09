c42d12514dced6f97ad0df032af9c9efec1038ee
Jorge Ismael <vazlop.jorge@gmail.com>
2020-03-09 11:44:45 -0600

    avance de las cards

---
+++ js/ejemplo1.js
@@ -0,0 +1,11 @@
+var nombre = prompt("Proporciona tu nombre: ");
+var apellido = prompt("Proporciona tu primer apellido: ");
+var nombreCompleto = alert(nombre +" "+ apellido);
+
+var edad = prompt("Cual es tu edad: ");
+var datos = alert("Tu nombre es: " +" " + (nombre +" "+ apellido) +" "+ ("Y tu edad es: ") +" "+ (edad) +" "+ ("Y tu año de nacimiento es:") +" "+ (2020 - edad));
+
+
+var musica = prompt("Que musica te gusta? ");
+
+var genero = alert(nombreCompleto +" "+ ("Tu Genero de musica es: " +" "+ ))
